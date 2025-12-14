eccca52b17f4819ca6d98425de321873eecfa966
pratikphadte <pratikphadte19@gmail.com>
2025-12-14 21:43:19 +0100

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mada-frontend/',
+  base: '/mada-deepfake-detector/',
   server: {
     port: 3000,
     proxy: {
